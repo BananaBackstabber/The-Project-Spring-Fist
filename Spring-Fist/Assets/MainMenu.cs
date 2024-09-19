506f1c1b4b89a7ab01e57e3ee284c086575c9e22
BananaBackstabber <unfoundlake@gmail.com>
2024-09-19 18:00:40 +0100

    Menu Screen and score screen added
    -Add a basic main menu system, needs to be touched up with assets and a plan need to be make on the settings menu, what basic things could I add to it to give it some flare
    
    -Start on more for the score screen, needs a design for the screen first be I start coding the scoring system and any other features for the screen

--- Spring-Fist/Assets/MainMenu.cs
+++ Spring-Fist/Assets/MainMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -10,12 +11,41 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    public void StartGame() 
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    } 
+
+    public void ExitGame() 
+    {
+
+
+         
+        SceneManager.LoadScene(2);
+    
+    
+    }
+
+    public void Settings() 
+    {
+       //Load settings screen stuff
+    
+    }
+
+    public void HowTo() 
+    {
+       //could be another scene
+    
     }
 }
